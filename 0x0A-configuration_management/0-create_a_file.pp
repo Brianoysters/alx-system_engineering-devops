3ca20e5ae03db5f72c9d2d048b6f54c5e7d69b25
Brianoysters <brianoysters@gmail.com>
2024-04-22 09:53:02 -0700

    manage

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# crete a file meeting the required details
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
